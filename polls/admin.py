6864780be9b81470d4c99fa493c5c78c2db379d0
nzupec <zupec.nejc@gmail.com>
2017-07-12 18:26:59 +0200

    Show models in the Admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Field
+from polls.models import Poll
+from polls.models import Template
+from polls.models import Vote
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('title', 'template')
+
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Field)
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('title', 'template')
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('poll', 'field', 'value')
