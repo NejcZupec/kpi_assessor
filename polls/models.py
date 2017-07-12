085d6fa743302c8d431f233da5cf8a9c3407ef73
nzupec <zupec.nejc@gmail.com>
2017-07-12 18:07:09 +0200

    Implement a base model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=255)
+    template = models.ForeignKey('polls.Template')
+
+
+class Template(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Field(models.Model):
+    title = models.CharField(max_length=255)
+    template = models.ForeignKey('polls.Template')
+
+
+class Vote(models.Model):
+    poll = models.ForeignKey('polls.Poll')
+    field = models.ForeignKey('polls.Field')
+    value = models.IntegerField()
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
 
-# Create your tests here.
+from polls.models import Field
+from polls.models import Poll
+from polls.models import Template
+from polls.models import Vote
+
+
+class TestPoll(TestCase):
+
+    def test_normal_poll_creation(self):
+        template = Template(title='template')
+        poll = Poll(
+            title='poll',
+            template=template,
+        )
+        self.assertEqual(poll.title, 'poll')
+        self.assertEqual(poll.template, template)
+
+
+class TestTemplate(TestCase):
+
+    def test_normal_template_creation(self):
+        template = Template(
+            title='template',
+        )
+        self.assertEqual(template.title, 'template')
+
+
+class TestField(TestCase):
+
+    def test_normal_field_creation(self):
+        template = Template(title='template')
+        field = Field(
+            title='field',
+            template=template,
+        )
+        self.assertEqual(field.title, 'field')
+        self.assertEqual(field.template, template)
+
+
+class TestVote(TestCase):
+
+    def test_normal_vote_creation(self):
+        template = Template(title='template')
+
+        field = Field(
+            title='field',
+            template=template,
+        )
+
+        poll = Poll(
+            title='poll',
+            template=template,
+        )
+
+        vote = Vote(
+            poll=poll,
+            field=field,
+            value=2,
+        )
+
+        self.assertEqual(vote.poll, poll)
+        self.assertEqual(vote.field, field)
+        self.assertEqual(vote.value, 2)
